9b3142576cfe8f2ef873c3a30d8ca9597e2736e4
jessicacfsb <jessica.cfsb@gmail.com>
2021-10-08 20:06:34 -0300

    exercicio02

---
+++ aula 003/script.js
@@ -0,0 +1,12 @@
+//aula sobre obj
+
+var person = {
+    name: 'Jéssica',
+    age: 22,
+    sex: 'Female'
+}
+
+console.log (person)
+console.log (person.name)
+console.log (person.age)
+console.log (person.sex)
